36179cd933ec9e695af9eb43ae9aa3b9b1144395
watchafilm <watcharapon.pck@gmail.com>
2025-07-21 14:56:07 +0700

    ห้ามคลิกขวา

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect } from 'react';
 import { Header } from '@/components/Header';
 
 export default function MainLayout({
@@ -5,6 +8,18 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const handleContextMenu = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+
+    document.addEventListener('contextmenu', handleContextMenu);
+
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
+
   return (
     <>
       <Header />
